b215d5944905a69f9ab814ba9136556429afa6c7
asqito <asqito@gmail.com>
2020-07-02 11:03:36 +0900

    word-tester 0.1 version

---
+++ word_tester01.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+# 파일 불러오기
+word = pd.read_excel('English_words01.xlsx', sheet_name='words')
+
+# 테스트 Day 선택하기
+day_no = input('단어 연습할 Day를 선택해 주세요:')
+
+# 점수 초기화
+score = 0
+
+# 테스트 페이지 선택하기
+if day_no == "1":
+    j = 0
+    k = 9
+
+elif day_no == "2":
+    j = 10
+    k = 19
+
+elif page_no == "3":
+    j = 20
+    k = 29
+
+# 테스트
+while j <= k:
+
+    print(word['뜻'][j])
+    answer = input(" >> 영어 단어: ")
+
+    # 정답인 경우
+    if answer == word['단어'][j]:
+        print("정답입니다!")
+        score += 1
+
+    # 오답인 경우
+    else:
+        print("오답, 정답은 " + word['단어'][j])
+
+    print("\n")
+    j += 1
+
+# 테스트 종료
+print("당신의 점수는", "%.1f" % ((score * 100) / (k + 1)) + "점 입니다.")
